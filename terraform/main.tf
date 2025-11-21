e38f825b54b3c27557f5aa8fee08fa9e3fbac39d
olawale lateef <olawalelateef9@gmail.com>
2025-11-21 11:17:18 -1200

    adding terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
-#place holder for Terraform
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+resource "aws_instance" "nginx-node" {
+  ami                    = "ami-0eeda20e2e0c4bba1"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0786335ea9e3bbe1d"
+  vpc_security_group_ids = ["sg-01ce2395a89767248"]
+  key_name               = "MasterClass9"
+
+  tags = {
+    Name = "terraform-nginx-node"
+  }
+}
